25fab86fedc415d4f47f05f2a0f7f6f3c5e31335
Krauß, Peter <peter.krauss@asideas.de>
2015-01-05 09:12:41 +0100

    adding apache license

--- src/com/google/glassware/AuthSettings.java
+++ src/com/google/glassware/AuthSettings.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class AuthSettings {
-	public static String CLIENT_ID = "571093447971-all1qtpe17v0u5epl7uo49ca06lqomip.apps.googleusercontent.com";
-	public static String CLIENT_SECRET = "CvIsQeM7KmPqnyVAxp9ev4V3";
+	public static String CLIENT_ID = "Your-Client-ID from App Engine";
+	public static String CLIENT_SECRET = "Your-Client-Secret from App Engine";
 	
 	public static final List<String> GLASS_SCOPE = 
 			  Arrays.asList("https://www.googleapis.com/auth/glass.timeline",
